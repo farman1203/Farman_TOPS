0e12abdd595c59e1f5678800ed4609410ddb7fce
farman1203 <ansarifarman1203@gmail.com>
2025-11-09 12:19:16 +0530

    first commit

--- Extra_Task/React/myfirstreact/src/App.js
+++ Extra_Task/React/myfirstreact/src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./Layout_src/Component/Footer";
 import Header from "./Layout_src/Component/Header";
+import About from "./Layout_src/pages/About";
 import Home from "./Layout_src/pages/Home";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div>
       {/* <Header/> */}
       {/* <Footer/> */}
-
+      {/* <About /> */}
       <Home />
     </div>
   );
