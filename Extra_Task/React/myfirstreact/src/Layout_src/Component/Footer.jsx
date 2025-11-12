c964085cedd8a8ec1f3e5d104c7c1cd710cff5d2
farman1203 <ansarifarman1203@gmail.com>
2025-11-12 12:42:15 +0530

    first commit

--- Extra_Task/React/myfirstreact/src/Layout_src/Component/Footer.jsx
+++ Extra_Task/React/myfirstreact/src/Layout_src/Component/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <div class="mt-5 p-4 bg-dark text-white text-center">
-                <p>Footer</p>
+            <div class="mt-5  p-4 bg-dark text-white text-center">
+                <p class="p-2">Footer</p>
             </div>
         </div>
     )
