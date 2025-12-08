ef311feb641d8d16a34658e26f42db4c17c8416a
farman1203 <ansarifarman1203@gmail.com>
2025-12-08 15:21:01 +0530

    first commit

---
+++ Extra_Task/React_JS/01-Project/Zay_shop/src/Admin/Pages/Manage_product.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Manage_product() {
+    return (
+        <div>
+            <div className="container-fluid bg-light py-5">
+                <div className="col-md-6 m-auto text-center">
+                    <h1 className="h1">Manage Product</h1>
+
+                </div>
+            </div>
+            <div className="container py-5">
+                <div className="row py-5">
+                    <div className="col-md-9 m-auto">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Prod Name</th>
+                                    <th>Image</th>
+                                    <th className='text-center'>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Men</td>
+                                    <td><img src="https://www.w3schools.com/bootstrap5/img_avatar3.png" width="50px"alt="" /></td>
+                                    <td className='text-center'>
+                                        <button className='btn btn-danger me-2'>Delete</button>
+                                        <button className='btn btn-primary'>Edit</button>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Manage_product
--- Extra_Task/React_JS/01-Project/Zay_shop/src/App.jsx
+++ Extra_Task/React_JS/01-Project/Zay_shop/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './Website/Component/Header'
 import Home from './Website/Pages/Home'
 import Footer from './Website/Component/Footer'
@@ -9,26 +9,43 @@ import Shop from './Website/Pages/Shop'
 import Shop_single from './Website/Pages/Shop_single'
 import Signup from './Website/Pages/Signup'
 import Login from './Website/Pages/Login'
+import AHeader from './Admin/Component/AFooter'
+import AFooter from './Admin/Component/AHeader'
+import Admin_login from './Admin/Pages/Admin_login'
+import Dashboard from './Admin/Pages/Dashboard'
+import Add_categories from './Admin/Pages/Add_categories'
+import Manage_categories from './Admin/Pages/Manage_categories'
+import Add_product from './Admin/Pages/Add_product'
+import Manage_product from './Admin/Pages/Manage_product'
+import Manage_contact from './Admin/Pages/Manage_contact'
+import Manage_customer from './Admin/Pages/Manage_customer'
 
 const App = () => {
   return (
     <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<><Header/><Home /><Footer/></>}></Route>
-            <Route path='/about' element={<><Header/><About /><Footer/></>}></Route>
-            <Route path='/contact' element={<><Header/><Contact /><Footer/></>}></Route>
-            <Route path='/shop' element={<><Header/><Shop /><Footer/></>}></Route>
-            <Route path='/shop_single' element={<><Header/><Shop_single /><Footer/></>}></Route>
-            <Route path='/login' element={<><Header/><Login /><Footer/></>}></Route>
-            <Route path='/signup' element={<><Header/><Signup /><Footer/></>}></Route>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<><Header /><Home /><Footer /></>}></Route>
+          <Route path='/about' element={<><Header /><About /><Footer /></>}></Route>
+          <Route path='/contact' element={<><Header /><Contact /><Footer /></>}></Route>
+          <Route path='/shop' element={<><Header /><Shop /><Footer /></>}></Route>
+          <Route path='/shop_single' element={<><Header /><Shop_single /><Footer /></>}></Route>
+          <Route path='/login' element={<><Header /><Login /><Footer /></>}></Route>
+          <Route path='/signup' element={<><Header /><Signup /><Footer /></>}></Route>
 
 
 
+          <Route path="/admin-login" element={<><Admin_login /></>}></Route>
+          <Route path="/dashboard" element={<><AHeader /><Dashboard /><AFooter /></>}></Route>
+          <Route path="/add_categories" element={<><AHeader /><Add_categories /><AFooter /></>}></Route>
+          <Route path="/manage_categories" element={<><AHeader /><Manage_categories /><AFooter /></>}></Route>
+          <Route path="/add_product" element={<><AHeader /><Add_product /><AFooter /></>}></Route>
+          <Route path="/manage_product" element={<><AHeader /><Manage_product /><AFooter /></>}></Route>
+          <Route path="/manage_contact" element={<><AHeader /><Manage_contact /><AFooter /></>}></Route>
+          <Route path="/manage_customer" element={<><AHeader /><Manage_customer /><AFooter /></>}></Route>
 
-            
-          </Routes>
-        </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
