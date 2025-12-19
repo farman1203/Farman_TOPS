635ca284e7547888bc2e20bf811bf2c9777cd288
farman1203 <ansarifarman1203@gmail.com>
2025-12-19 22:02:47 +0530

    first commit

--- Extra_Task/React_JS/01-Project/Gallery_project/src/App.jsx
+++ Extra_Task/React_JS/01-Project/Gallery_project/src/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
+    const [userData, setUserData] = useState([])
+
     const getdata = async () =>{
       const response=await axios.get('https://fakestoreapi.com/products')
-        console.log(response.data);
+       setUserData(response.data)
+       console.log(response);
+       
     }
-     
-      
     
+    let printuserdata = "No user data."
+    
+    if(userData.length>0){
+        printuserdata = userData.map(function(elem){
+          return <div>
+            <img src={elem.image} alt="" />
+          </div>
+        })
+    }
   return (
     <div>
         <button onClick={getdata}>Get Data</button>
+
+
+        <div>
+          {printuserdata}
+        </div>
     </div>
   )
 }
