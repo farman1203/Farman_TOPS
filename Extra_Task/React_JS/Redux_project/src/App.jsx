1f1ddbf8274e1a5b7579d4b1a75ffdae93dd965b
farman1203 <ansarifarman1203@gmail.com>
2026-02-14 11:00:05 +0530

    first commit

---
+++ Extra_Task/React_JS/Redux_project/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import AHeader from './Admin/Component/AFooter'
+import AFooter from './Admin/Component/AHeader'
+// import Admin_login from './Admin/Pages/Admin_login'
+import Dashboard from './Admin/Pages/Dashboard'
+import Add_categories from './Admin/Pages/Add_categories'
+import Manage_categories from './Admin/Pages/Manage_categories'
+import Add_product from './Admin/Pages/Add_product'
+import Manage_product from './Admin/Pages/Manage_product'
+import Manage_contact from './Admin/Pages/Manage_contact'
+import Manage_customer from './Admin/Pages/Manage_customer'
+// import Pagenotfound from './Website/Pages/Pagenotfound'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<><AFooter /><Dashboard /><AHeader/></>}></Route>
+          {/* <Route path="/admin-login" element={<><Admin_login /></>}></Route> */}
+          <Route path="/add_categories" element={<><AFooter /><Add_categories /><AHeader/></>}></Route>
+          <Route path="/manage_categories" element={<><AFooter /><Manage_categories /><AHeader/></>}></Route>
+          <Route path="/add_product" element={<><AFooter /><Add_product /><AHeader/></>}></Route>
+          <Route path="/manage_product" element={<><AFooter /><Manage_product /><AHeader/></>}></Route>
+          <Route path="/manage_contact" element={<><AFooter /><Manage_contact /><AHeader/></>}></Route>
+          <Route path="/manage_customer" element={<><AFooter /><Manage_customer /><AHeader/></>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
