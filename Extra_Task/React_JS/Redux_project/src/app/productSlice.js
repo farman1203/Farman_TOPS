3cff11c0c197bd089d4145bb4e7d010557c8dbe9
farman1203 <ansarifarman1203@gmail.com>
2026-02-20 16:32:24 +0530

    first commit

---
+++ Extra_Task/React_JS/Redux_project/src/app/productSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice=createSlice({
+    name:'productSlice',
+    initialState:{
+        product:[],
+        name:"farman"
+    },
+    reducers:{
+        
+    }
+})
